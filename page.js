e966e10a8f2529a6e201ed32a233fcc9c9237c16
Juppsix <julien.postecc@gmail.com>
2023-01-09 14:25:38 +0000

    ouiouioui

--- page.js
+++ page.js
@@ -1,4 +1,14 @@
 function maskey() 
 {
+    let btn1 = document.getElementById("bi-caret-down");
+    let liste1 = document.getElementById("close");
     
+    btn1.addEventListener("click", function(event){
+        // liste1.style.display = none;
+        for (i = 0; i < liste1.length; i++) {
+            liste1[i].style.display = 'none';
+        }
+        document.getElementById('spanish').style.display = 'block';
+
+    });        
 }
